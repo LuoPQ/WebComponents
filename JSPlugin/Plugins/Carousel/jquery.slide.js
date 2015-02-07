8685721ed0e9265d76235a6783b940fa86cbe2fe
lrzw32 <lrzw32@126.com>
2015-02-07 09:03:06 +0800

    slide

---
+++ JSPlugin/Plugins/Carousel/jquery.slide.js
@@ -0,0 +1,41 @@
+﻿; (function ($) {
+    "use strict";
+
+    var defaults = {
+        "speed": 1000,
+        "direction": "left",
+        "type": "carousel",//carousel or gallery
+    }
+
+    $.fn.slide = function (options) {
+        options = $.extend(defaults, options || {});
+
+        var $container = $(this);
+        var $slider = $container.find(".slider");
+
+        $container.css({
+            "overflow": "hidden",
+            "position": "relative"
+        });
+
+        var sliderCss = {
+            "position": "absolute"
+        }
+        switch (options.direction) {
+            case "top":
+            case "bottom":
+                sliderCss = $.extend(sliderCss, { "height": "100%" });
+                break;
+            case "left":
+            case "right":
+                sliderCss = $.extend(sliderCss, { "width": "3000px" });
+                break;
+            default:
+
+        }
+        $slider.css(sliderCss);
+
+
+    };
+
+})($);
