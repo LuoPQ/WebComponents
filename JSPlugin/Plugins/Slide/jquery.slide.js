f82b40c90a8ea7f92de42015628ebacba7231df1
lrzw32 <lrzw32@126.com>
2015-04-23 18:27:40 +0800

    newslide

---
+++ JSPlugin/Plugins/Slide/jquery.slide.js
@@ -0,0 +1,175 @@
+﻿; (function ($) {
+    "use strict";
+
+    var defaults = {
+        "scrollTimeSpan": 500,
+        "scrollTime": 4500,
+        "direction": "left",
+        "type": "gallery",//carousel or gallery,
+        "scrollCount": 1,
+        "scrollBack": true,
+        "onScrollToBottom": null
+    }
+
+    $.fn.slide = function (options) {
+        options = $.extend(defaults, options || {});
+
+        var $container = $(this);
+        var $slider = $container.find(".slider");
+        var $sliderItems = $slider.children();
+
+        var timer = null;//计时器
+        var index = 0;//开始滚动的索引
+        var count = 0;//一次完成滚动所需要的次数
+
+        var containerLength = 0;
+        var scrollLength = 0;
+
+        $container.css({
+            "height": options.containerHeight || $container.height(),
+            "width": options.containerWidth || $container.width(),
+            "overflow": "hidden",
+            "position": "relative"
+        });
+
+        var sliderCss = {
+            "position": "absolute"//保证Slide的position为absolute，必须脱离文档流才能滚动
+        }
+
+        //一开始停止可能存在的动画
+        $slider.stop();
+
+        switch (options.direction) {
+            case "top":
+            case "bottom":
+                sliderCss = $.extend(sliderCss, { "height": "100%" });
+                containerLength = $container.height();
+                count = $sliderItems.length / options.scrollCount;
+
+                //横向滚动的距离等于元素的宽度乘以滚动的元素个数
+                scrollLength = $sliderItems.first().outerHeight(true) * options.scrollCount;
+
+                //滑入停止动画，滑出开始动画.
+                $container.hover(function () {
+                    stopTimer();
+                }, function () {                                        
+                    initTimer();
+                }).trigger("mouseleave");
+
+                sliderCss = $.extend(sliderCss,
+                    {
+                        "top": -scrollLength * index + "px" //改变 marginTop 属性的值达到轮播的效果
+                    });
+                break;
+            case "left":
+            case "right":
+                containerLength = $container.width();
+                count = $sliderItems.length / options.scrollCount;
+
+                //横向滚动的距离等于元素的宽度乘以滚动的元素个数
+                scrollLength = $sliderItems.first().outerWidth(true) * options.scrollCount;
+
+                //滑入停止动画，滑出开始动画.
+                $container.hover(function () {
+                    stopTimer();
+                }, function () {
+                    initTimer();
+                }).trigger("mouseleave");
+
+                sliderCss = $.extend(sliderCss,
+                    {
+                        "width": $sliderItems.length * $sliderItems.first().outerWidth(true),
+                        "left": -scrollLength * index + "px" //改变 marginTop 属性的值达到轮播的效果
+                    });
+
+                break;
+            default:
+
+        }
+        $slider.css(sliderCss);
+
+        ///初始化计时器
+        function initTimer() {
+            timer = setInterval(function () {
+                showImg(index)
+                index++;
+                if (index >= count) {//最后一张图片之后
+                    //如果支持反向滚动，则设置index为0
+                    if (options.scrollBack) {
+                        index = 0;
+                    }
+
+                    //如果有滚到底部的回调函数
+                    if (options.onScrollToBottom) {
+                        index = 0;
+                        options.onScrollToBottom();
+                    }
+                }
+            }, options.scrollTimeSpan);
+        }
+
+        function stopTimer() {
+            $slider.stop(true, false);
+            clearInterval(timer);
+        }
+
+        //重启计时器
+        function restartTimer(index) {
+            clearInterval(timer);
+            showImg(index);
+            initTimer();
+        }
+
+        function showImg(index) {
+
+            //当滚动的距离与最大滚动距离之差小于容器的尺寸时
+            //会出现空白区域，所以需要设置一个最大的滚动距离
+            if (scrollLength * (count - index) < containerLength) {
+                var maxScrollLength = containerLength - scrollLength * count;
+            }
+            var resultValue = maxScrollLength ? maxScrollLength : (-scrollLength * index) + "px";
+
+            var animateCss = null;
+
+            switch (options.direction) {
+                case "top":
+                case "bottom":
+                    animateCss = {
+                        "top": resultValue
+                    }
+                    break;
+                case "left":
+                case "right":
+                    animateCss = {
+                        "left": resultValue
+                    }
+                    break;
+                default:
+
+            }
+            $slider.animate(animateCss, options.scrollTime);//stop(true, false)
+        }
+
+
+        $(".slideLeft").on("click", function () {
+            if (index > 0) {
+                --index
+            }
+            else {
+                index = count;
+            }
+            restartTimer(index);
+        });
+
+        $(".slideRight").on("click", function () {
+            if (index < count) {
+                ++index;
+            }
+            else {
+                index = 0;
+            }
+            restartTimer(index);
+        });
+    };
+
+})($);
