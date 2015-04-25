1b0b975febf9baa435175a27a4fb8e9152801319
lrzw32 <lrzw32@126.com>
2015-04-25 17:25:56 +0800

    carousel

---
+++ JSPlugin/Plugins/Carousel/jquery.carousel.js
@@ -0,0 +1,79 @@
+﻿; (function ($) {
+    "use strict";
+
+    var defaults = {
+        "switchSpeed": 4000,
+        "fadeSpeed": 800
+    }
+
+    $.fn.carousel = function (options) {
+        options = $.extend(defaults, options || {});
+
+        var timer = null;
+
+        var $imgList = $(this);
+        var $btnList = $(options.btnSelector);
+        var length = $imgList.length;
+        var index = 0;
+
+        for (var i = 0; i < length; i++) {
+            $imgList.eq(i).css({
+                "zIndex": length - i,
+                "position": "absolute"
+            });
+
+            $btnList.eq(i).css({
+                "z-index": 999
+            })
+        }
+        function startTimer() {
+            timer = setTimeout(function () {
+                $imgList.eq(index).fadeOut(options.fadeSpeed);
+                if (index < length - 1) {
+                    index++;
+                }
+                else {
+                    index = 0;
+                }
+                $imgList.eq(index).fadeIn(options.fadeSpeed);
+                $btnList.eq(index).addClass("select").siblings().removeClass("select");
+                startTimer();
+            }, options.switchSpeed)
+        }
+
+        function stopTimer(hoverIndex, hoverEle) {
+            clearTimeout(timer);
+            $imgList.eq(index).stop().fadeOut(options.fadeSpeed);
+            index = hoverIndex;
+            $imgList.eq(index).fadeIn(options.fadeSpeed);
+            $(hoverEle).addClass("select").siblings().removeClass("select");
+        }
+
+        startTimer();
+
+        $imgList.each(function (i, ele) {
+            $(ele).on({
+                "mouseover": function () {
+                    clearTimeout(timer);
+                },
+                "mouseout": function () {
+                    startTimer();
+                }
+            })
+        });
+
+        $btnList.each(function (i, ele) {
+            $(ele).on({
+                "mouseover": function () {
+                    stopTimer(i, ele);
+                },
+                "mouseout": function () {
+                    startTimer();
+                }
+            })
+        });
+
+
+    };
+
+})($);
