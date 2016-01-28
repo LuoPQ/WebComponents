11c6b18778dbc22ce93aab9e53defa9b6e8b1531
LuoPQ <lrzw32@126.com>
2016-01-28 15:29:56 +0800

    pager

--- JSPlugin/Plugins/Pager/jquery.pager.js
+++ JSPlugin/Plugins/Pager/jquery.pager.js
@@ -1,4 +1,10 @@
-(function ($) {
+/**
+ * jquery.pager.js v0.1.0
+ * MIT License
+ * author info pls visit: http://luopq.com
+ * for more info pls visit: https://github.com/LuoPQ/jquery.pager.js
+ */
+; (function ($, window, document, undefined) {
     "use strict";
     var defaults = {
         pageIndex: 0,
@@ -6,15 +12,22 @@
         itemCount: 50,
         maxButtonCount: 7,
         prevText: "上一页",
-        nextText: "下一页"
+        nextText: "下一页",
+        buildPageUrl: null,
+        onPageChanged: null
     };
 
     function Pager($ele, options) {
         this.$ele = $ele;
         this.options = options = $.extend(defaults, options || {});
+        this.init();
     }
     Pager.prototype = {
         constructor: Pager,
+        init: function () {
+            this.renderHtml();
+            this.bindEvent();
+        },
         renderHtml: function () {
             var options = this.options;
 
@@ -23,7 +36,7 @@
 
             //生成上一页的按钮
             if (options.pageIndex > 0) {
-                html.push('<a page="' + (options.pageIndex - 1) + '" href="' + onGetPageUrl(options.pageIndex + 1) + '" class="flip">' + options.prevText + '</a>');
+                html.push('<a page="' + (options.pageIndex - 1) + '" href="' + this.buildPageUrl(options.pageIndex + 1) + '" class="flip">' + options.prevText + '</a>');
             } else {
                 html.push('<span class="flip noPage">' + options.prevText + '</span>');
             }
@@ -38,7 +51,7 @@
 
             // 第一页
             if (startIndex > 0) {
-                html.push("<a href='" + onGetPageUrl(0) + "' page='" + 0 + "'>1</a> ");
+                html.push("<a href='" + this.buildPageUrl(0) + "' page='" + 0 + "'>1</a> ");
                 html.push("<span>...</span>");
             }
 
@@ -47,19 +60,19 @@
                 if (options.pageIndex == i) {
                     html.push('<span class="curPage">' + (i + 1) + '</span>');
                 } else {
-                    html.push('<a page="' + i + '" href="' + onGetPageUrl(options.pageIndex + 1) + '">' + (i + 1) + '</a>');
+                    html.push('<a page="' + i + '" href="' + this.buildPageUrl(options.pageIndex + 1) + '">' + (i + 1) + '</a>');
                 }
             }
 
             // 最后一页
             if (endIndex < options.pageCount - 1) {
                 html.push("<span>...</span> ");
-                html.push("<a href='" + onGetPageUrl(options.pageCount - 1) + "' page='" + (options.pageCount - 1) + "'>" + options.pageCount + "</a> ");
+                html.push("<a href='" + this.buildPageUrl(options.pageCount - 1) + "' page='" + (options.pageCount - 1) + "'>" + options.pageCount + "</a> ");
             }
 
             //生成下一页的按钮
             if (options.pageIndex < options.pageCount - 1) {
-                html.push('<a page="' + (options.pageIndex + 1) + '" href="' + onGetPageUrl(options.pageIndex + 1) + '" class="flip">' + options.nextText + '</a>');
+                html.push('<a page="' + (options.pageIndex + 1) + '" href="' + this.buildPageUrl(options.pageIndex + 1) + '" class="flip">' + options.nextText + '</a>');
             } else {
                 html.push('<span class="flip noPage">' + options.nextText + '</span>');
             }
@@ -68,10 +81,29 @@
         },
         bindEvent: function () {
             var that = this;
-            this.$ele.on("click", "a", function () {
-                options.pageIndex = parseInt($(this).attr("page"), 10);
+            that.$ele.on("click", "a", function () {
+                that.options.pageIndex = parseInt($(this).attr("page"), 10);
                 that.renderHtml();
+                that.options.onPageChanged && that.options.onPageChange(that.options.pageIndex);
             })
+        },
+        buildPageUrl: function () {
+            if ($.isFunction(this.options.buildPageUrl)) {
+                return this.options.buildPageUrl(pageIndex);
+            }
+            return "javascript:;";
+        },
+        getPageIndex: function () {
+            return this.options.pageIndex;
+        },
+        setPageIndex: function (pageIndex) {
+            this.options.pageIndex = pageIndex;
+            this.renderHtml();
+        },
+        setItemCount: function (itemCount) {
+            this.options.pageIndex = 0;
+            this.options.itemCount = itemCount;
+            this.renderHtml();
         }
     };
 
@@ -79,93 +111,7 @@
     $.fn.pager = function (options) {
         options = $.extend(defaults, options || {});
 
-        var element = $(this);
-
-        function renderHtml() {
-            options.pageCount = Math.ceil(options.itemCount / options.pageSize);
-            var html = [];
-
-            //生成上一页的按钮
-            if (options.pageIndex > 0) {
-                html.push('<a page="' + (options.pageIndex - 1) + '" href="' + onGetPageUrl(options.pageIndex + 1) + '" class="flip">' + options.prevText + '</a>');
-            } else {
-                html.push('<span class="flip noPage">' + options.prevText + '</span>');
-            }
-
-
-            //这里是关键
-            //临时的起始页码中间页码，当页码数量大于显示的最大按钮数时使用
-            var tempStartIndex = options.pageIndex - Math.floor(options.maxButtonCount / 2) + 1;
-
-            //两种方式计算首尾的页码：
-            //先计算终止页码，通过max计算一排按钮中的第一个按钮的页码，然后计算出页数量
-            var endIndex = Math.min(options.pageCount, Math.max(0, tempStartIndex) + options.maxButtonCount) - 1;
-            var startIndex = Math.max(0, endIndex - options.maxButtonCount + 1);
-
-            //先计算起始页码，通过max计算第一个按钮的页码，注意midPageIndex和pageIndex之间的关系
-            // 第一页
-            if (startIndex > 0) {
-                html.push("<a href='" + onGetPageUrl(0) + "' page='" + 0 + "'>1</a> ");
-                html.push("<span>...</span>");
-            }
-
-            //生成页码按钮
-            for (var i = startIndex; i <= endIndex; i++) {
-                if (options.pageIndex == i) {
-                    html.push('<span class="curPage">' + (i + 1) + '</span>');
-                } else {
-                    html.push('<a page="' + i + '" href="' + onGetPageUrl(options.pageIndex + 1) + '">' + (i + 1) + '</a>');
-                }
-            }
-
-            // 最后一页
-            if (endIndex < options.pageCount - 1) {
-                html.push("<span>...</span> ");
-                html.push("<a href='" + onGetPageUrl(options.pageCount - 1) + "' page='" + (options.pageCount - 1) + "'>" + options.pageCount + "</a> ");
-            }
-
-            //生成下一页的按钮
-            if (options.pageIndex < options.pageCount - 1) {
-                html.push('<a page="' + (options.pageIndex + 1) + '" href="' + onGetPageUrl(options.pageIndex + 1) + '" class="flip">' + options.nextText + '</a>');
-            } else {
-                html.push('<span class="flip noPage">' + options.nextText + '</span>');
-            }
-
-
-        }
-
-
-        function onGetPageUrl(pageIndex) {
-            if ($.isFunction(options.onGetPageUrl)) {
-                return options.onGetPageUrl(pageIndex);
-            }
-            return "javascript:;";
-        }
-
-
-        //声明Pager对象
-        function Pager() {
-            renderHtml();
-        };
-
-        //公开的方法
-        Pager.prototype = {
-            getPageIndex: function () {
-                return options.pageIndex;
-            },
-            setPageIndex: function (pageIndex) {
-                options.pageIndex = pageIndex;
-                renderHtml();
-            },
-            setItemCount: function (itemCount) {
-                options.pageIndex = 0;
-                options.itemCount = itemCount;
-                renderHtml();
-            }
-
-        }
-
-        return new Pager();
+        return new Pager($(this), options);
     }
 
-})(jQuery);
+})(jQuery, window, document);
